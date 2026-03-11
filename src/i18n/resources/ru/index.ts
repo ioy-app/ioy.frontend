64f0df5155512c3c7d656b6aa64e2b9b14340938
wmgcat <developer@wmgcat.net>
2026-03-11 21:56:37 +0300

    IOY-49: Added russian language

--- src/i18n/resources/ru/index.ts
+++ src/i18n/resources/ru/index.ts
@@ -4,11 +4,20 @@ import footer from "./footer.json";
 import profile from "./profile.json";
 import auth from "./auth.json";
 import dashboard from "./dashboard.json";
+import jams from "./jams.json";
 import games from "./games.json";
-import notify from "./notify.json";
-import home from "./home.json";
 import codes from "./codes.json";
+import home from "./home.json";
+import terms from "./terms.json";
 import about from "./about.json";
+
+import order from "./components/order.json";
+import helps from "./components/helps.json";
+import notify from "./components/notify.json";
+import errors from "./components/errors.json";
+import tabs from "./components/tabs.json";
+import report from "./components/report.json";
+
 
 const lang = {
 	buttons,
@@ -16,12 +25,19 @@ const lang = {
 	footer,
 	profile,
 	auth,
+	order,
 	dashboard,
+	jams,
 	games,
-	notify,
-	home,
 	codes,
-	about,
+	home,
+	helps,
+	terms,
+	notify,
+	errors,
+	tabs,
+	report,
+	about
 };
 
 export default lang;
