4dcaabecf9dc7d0ef57d79cd28daad387b742db6
wmgcat <developer@wmgcat.net>
2026-03-15 02:07:28 +0300

    IOY-18: Added modal report

--- src/pages/dashboard/content/reports.tsx
+++ src/pages/dashboard/content/reports.tsx
@@ -2,6 +2,7 @@ import confOrder from "@/configs/order.json";
 
 import {
 	BiBox,
+	BiCheck,
 	BiEditAlt,
 	BiPlus,
 	BiReply,
@@ -21,32 +22,23 @@ import {
 	useSearchParams,
 } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { games_paths } from "@/routes/games";
 import { paths } from "@/routes";
 import GameProps from "@/types/game";
 import { useQuery } from "@tanstack/react-query";
 import { FormProvider, useForm } from "react-hook-form";
-import { users_favorites } from "@/api/routes/users";
-import { useSelector } from "react-redux";
-import { StoreProps } from "@/stories";
 import { reports } from "@/api/routes/reports";
 import { UserProps } from "@/types";
+import { useModal } from "@/hooks";
 
 const Reports: React.FC = () => {
 	const { t } = useTranslation();
-	const navigator = useNavigate();
-	const [searchParams, setSearchParams] = useSearchParams();
-	const { login } = useSelector(
-		(state: StoreProps) => state.login,
-	);
+	const { modal } = useModal();
+	const [ searchParams, setSearchParams ] = useSearchParams();
 
 	const max = 10;
 	const current_page = Number(
 		searchParams.get("page") || 1,
 	);
-	const status = searchParams.get("status");
-	const sort = searchParams.get("sort");
-	const searchQS = searchParams.get("search");
 
 	const query = useQuery({
 		queryKey: [
@@ -63,11 +55,9 @@ const Reports: React.FC = () => {
 		},
 	});
 
-	const onSubmit = async (data) => {
-		const us = new URLSearchParams();
-		if (data.sort) us.set("sort", data.sort);
-		setSearchParams(us);
-	};
+	const handleOpen = (data, i) => {
+		modal("", (onClose) => <ReportAnswer onClose={onClose} data={data} />);
+	}
 
 	const methods = useForm();
 
@@ -76,11 +66,6 @@ const Reports: React.FC = () => {
 			methods.setValue("sort", searchParams.get("sort"));
 	}, [searchParams]);
 
-	const sorOptions = confOrder?.map((item) => {
-		item.label = t(item.label);
-		return item;
-	});
-
 	return (
 		<div className="w-full flex flex-col gap-4">
 			<Components.Table
@@ -88,86 +73,7 @@ const Reports: React.FC = () => {
 					{
 						title: t("dashboard.table.reports.target_id"),
 						dataIndex: "instance",
-						render: (instance, data) => {
-							switch (data.target_type) {
-								case "game":
-									return (
-										<Link
-											to={paths.games.details(instance?.id)}
-											className="group flex items-center gap-2 w-fit"
-										>
-											<Components.Game
-												dataSource={
-													{
-														id: instance?.id,
-														is_avatar: instance?.is_avatar,
-													} as GameProps
-												}
-												nolink
-												size={12}
-											/>
-											<p className="text-default group-hover:text-primary transition-colors cursor-pointer">
-												{instance?.title}
-											</p>
-										</Link>
-									);
-									break;
-								case "user":
-									return (
-										<Link
-											to={paths.users.details(instance?.id)}
-											className="group flex items-center gap-2 w-fit"
-										>
-											<Components.User
-												dataSource={
-													{
-														id: instance?.id,
-														is_avatar: instance?.is_avatar,
-													} as UserProps
-												}
-												login={instance?.login}
-												nolink
-												size={12}
-											/>
-											<p className="text-default group-hover:text-primary transition-colors cursor-pointer">
-												{instance?.login}
-											</p>
-										</Link>
-									);
-									break;
-								case "comment":
-									return (
-										<div className="flex items-start gap-2 w-full flex-col">
-											<Link
-												className="group flex gap-2 items-center"
-												to={paths.users.details(instance?.userdata?.login)}
-											>
-												<Components.User
-													dataSource={
-														{
-															id: instance?.userdata?.id,
-															is_avatar: instance?.userdata?.is_avatar,
-														} as UserProps
-													}
-													login={instance?.userdata?.login}
-													nolink
-													size={12}
-												/>
-												<p className="text-default group-hover:text-primary transition-colors cursor-pointer">
-													{instance?.userdata?.login}
-												</p>
-											</Link>
-											<p className="text-default border border-br px-4 py-2 rounded-xl w-full">
-												{instance?.comment}
-											</p>
-											<p className="text-placeholder w-full flex justify-end">{dayjs(instance?.date_created).format("HH:mm DD.MM.YYYY")}</p>
-										</div>
-									);
-								break;
-							}
-
-							return null;
-						},
+						render: (instance, data) => <RenderInstance instance={instance} data={data} />,
 					},
 					{
 						title: t("dashboard.table.reports.source_id"),
@@ -225,9 +131,7 @@ const Reports: React.FC = () => {
 					<>
 						<Components.Button
 							variant="second"
-							onClick={() => {
-								
-							}}
+							onClick={() => handleOpen(row, i)}
 						>
 							<BiReply />
 						</Components.Button>
@@ -251,5 +155,164 @@ const Reports: React.FC = () => {
 		</div>
 	);
 };
+
+const ReportAnswer: React.FC<{
+	onClose: () => void;
+	data: Record<string, any>;
+}> = ({
+	onClose,
+	data
+}) => {
+	const { t } = useTranslation();
+	const local = useForm();
+
+	const handleSubmit = async (data) => {
+		console.log(data);
+	}
+
+	return (
+		<FormProvider {...local}>
+			<form
+				className="flex w-full flex-col gap-4"
+				onSubmit={local.handleSubmit(handleSubmit)}
+			>
+				<p className="text-title text-center">{t("report.title")}</p>
+				<div className="flex gap-4 items-start">
+					<p className="text-placeholder">{t("dashboard.table.reports.target_id")}:</p>
+					<RenderInstance data={data} instance={data?.instance} />
+				</div>
+				<div className="flex gap-4 items-start">
+					<p className="text-placeholder">{t("dashboard.table.reports.source_id")}:</p>
+					<div className="flex gap-2 items-center">
+						<Components.User
+							dataSource={
+								{
+									id: data?.sourcedata?.id,
+									is_avatar: data?.sourcedata?.is_avatar,
+								} as UserProps
+							}
+							login={data?.sourcedata?.login}
+							nolink
+							size={12}
+						/>
+						<p className="text-default">
+							{data?.sourcedata?.login}
+						</p>
+					</div>
+				</div>
+				<div className="flex gap-4 items-start">
+					<p className="text-placeholder">{t("dashboard.table.reports.message")}:</p>
+					<p className="text-default">{data?.message}</p>
+				</div>
+				<div className="flex flex-col gap-4">
+					<Components.Checkbox
+						{...local.register("params.ban_instance_3d")}
+						placeholder={t("report.params.ban_instance_3d")}
+					/>
+					<Components.Checkbox
+						{...local.register("params.ban_instance_30d")}
+						placeholder={t("report.params.ban_instance_30d")}
+					/>
+					<Components.Checkbox
+						{...local.register("params.delete_instance")}
+						placeholder={t("report.params.delete_instance")}
+					/>
+				</div>
+				<Components.Textarea
+					{...local.register("answer")}
+					placeholder={t("report.placeholders.answer")}
+					label={t("report.labels.answer")}
+				/>
+				<div className="flex justify-end items-center gap-4">
+					<Components.Button
+						variant="primary"
+						htmlType="submit"
+					>
+						{t("buttons.save")}
+						<BiCheck />
+					</Components.Button>
+				</div>
+			</form>
+		</FormProvider>
+	);
+}
+
+const RenderInstance: React.FC<{}> = ({ instance, data }) => {
+	switch (data.target_type) {
+		case "game":
+			return (
+				<div
+					className="group flex items-center gap-2 w-fit"
+				>
+					<Components.Game
+						dataSource={
+							{
+								id: instance?.id,
+								is_avatar: instance?.is_avatar,
+							} as GameProps
+						}
+						nolink
+						size={12}
+					/>
+					<p className="text-default group-hover:text-primary transition-colors cursor-pointer">
+						{instance?.title}
+					</p>
+				</div>
+			);
+			break;
+		case "user":
+			return (
+				<div
+					className="group flex items-center gap-2 w-fit"
+				>
+					<Components.User
+						dataSource={
+							{
+								id: instance?.id,
+								is_avatar: instance?.is_avatar,
+							} as UserProps
+						}
+						login={instance?.login}
+						nolink
+						size={12}
+					/>
+					<p className="text-default group-hover:text-primary transition-colors cursor-pointer">
+						{instance?.login}
+					</p>
+				</div>
+			);
+			break;
+		case "comment":
+			return (
+				<div className="flex items-start gap-2 w-full flex-col">
+					<div
+						className="group flex gap-2 items-center"
+					>
+						<Components.User
+							dataSource={
+								{
+									id: instance?.userdata?.id,
+									is_avatar: instance?.userdata?.is_avatar,
+								} as UserProps
+							}
+							login={instance?.userdata?.login}
+							nolink
+							size={12}
+						/>
+						<p className="text-default group-hover:text-primary transition-colors cursor-pointer">
+							{instance?.userdata?.login}
+						</p>
+					</div>
+					<p className="text-default border border-br px-4 py-2 rounded-xl w-full">
+						{instance?.comment}
+					</p>
+					<p className="text-placeholder w-full flex justify-end">{dayjs(instance?.date_created).format("HH:mm DD.MM.YYYY")}</p>
+				</div>
+			);
+		break;
+	}
+
+	return null;
+}
 
 export default Reports;
