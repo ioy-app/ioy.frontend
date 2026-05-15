7f8a2fc96d7a95fe8a109577ae937506ec8400ac
wmgcat <developer@wmgcat.net>
2026-05-15 22:17:43 +0300

    IOY-122: Added redirect modal warning

--- src/components/base/linkifyText.tsx
+++ src/components/base/linkifyText.tsx
@@ -1,8 +1,11 @@
 import Tag from "@/components/custom/tag";
 import Linkify from "linkify-react";
 import { Opts } from "linkifyjs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "linkify-plugin-hashtag";
+import { useModal } from "@/hooks";
+import Button from "./button";
+import { useTranslation } from "react-i18next";
 
 const LinkifyText: React.FC<{
 	children: React.ReactNode;
@@ -11,10 +14,40 @@ const LinkifyText: React.FC<{
 	children,
 	className
 }) => {
+	const { modal } = useModal();
+	const { t } = useTranslation();
+
 	const renderLink = ({ attributes, content }) => (
 		<Link
 			to={attributes.href}
-			className="text-second"
+			className={`text-second text-nowrap ${className && className || ""}`}
+			onClick={(e) => {
+				e.preventDefault();
+				modal(() => (
+					<p className="text-default">
+						{t("modals.redirect", { url: attributes?.href })}
+					</p>
+				), (onClose) => (
+					<>
+						<Button
+							variant="danger"
+							onClick={() => onClose && onClose()}
+						>
+							{t("buttons.cancel")}
+						</Button>
+						<Button
+							variant="primary"
+							onClick={() => {
+								window?.open?.(attributes?.href, "_blank", "noopener,noreferrer");
+								onClose && onClose();
+							}}
+						>
+							{t("buttons.ok")}
+						</Button>
+					</>
+				));
+
+			}}
 		>
 			{content}
 		</Link>
@@ -23,21 +56,17 @@ const LinkifyText: React.FC<{
 
 	const options: Opts = {
 		render: {
-			link: renderLink,
+			url: renderLink,
 			hashtag: renderHashtag,
 		},
 		format: {
 			hashtag: (value: string) => value.substring(1),
-		},
-		target: "_blank",
-		className: `text-nowrap text-second ${(className && className) || ""}`,
+		}
 	};
 
 	return (
 		<Linkify options={options}>
-			<p
-				className={`flex text-default flex-row flex-wrap gap-2 ${(className && className) || ""}`}
-			>
+			<p className={`flex text-default flex-row flex-wrap gap-2 ${(className && className) || ""}`}>
 				{children}
 			</p>
 		</Linkify>
